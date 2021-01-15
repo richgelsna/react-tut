89355d59fb26194245464ffa99032a1c8843cf6d
Nathan Richgels <richgelsna.8@gmail.com>
2021-01-15 15:07:51 -0600

    Continued with tutorial, left off at 35:54, updated Notes.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-// All comments are supplementary notes to Notes.txt.  More in-depthness can be found there!
-
-// 'react' is the module, 'React' is the object being imported from the module.
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
-// jsx expression
-// Babel will then compile this into React.createElement
-// This is why you must: import React from 'react'
-// Babel will compile this code into:
-//  var element = React.createElement("h1", null, "Hello World");
-//
-// This element is a part of the "Virtual DOM"
-const element = <h1>Hello World!!</h1>;
-console.log(element);
 
-// This is a call to EXPLICITLY render in the "Real DOM".
-// The second argument is referencing the <div id="root"></div> in index.html (which is alredy part of the "Real" DOM).
-//
-// In a realworld application, instead of rendering a single element to the DOM, we'll render our "app component".
-// This app component is the root component of our application.  It includes children such as nav bar, side bar, profile, and what-not.
-// We'll have a tree of components, which will eventually produce complex markup.
-ReactDOM.render(element, document.getElementById("root"));
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
