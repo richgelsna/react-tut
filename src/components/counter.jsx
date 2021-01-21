deef1d3ce17c8d07dbc0ab6e6d68f03f75dc2b6c
Nathan Richgels <richgelsna.8@gmail.com>
2021-01-21 11:38:56 -0600

    Got up to 46:38 in the tutorial.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello World!</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
