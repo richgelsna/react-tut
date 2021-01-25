1f3810229e0ae3210754c834c6d7256ed2fd8cfc
Nathan Richgels <richgelsna.8@gmail.com>
2021-01-25 15:37:45 -0600

    Continued with tutorial up to point of 54:56 of the video.  Created new Notes file, as the other file was starting to get long.  Also created other research notes independent of tutorial.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,28 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    // this is an array:
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let countClasses = "badge m2 badge-";
+    countClasses += this.state.count === 0 ? "warning" : "primary";
+    return countClasses;
   }
 
   formatCount() {
